ec17a18f0eea624cf9acbd70ad241eed634e83b5
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-21 20:37:32 +0530

    Add files via upload

---
+++ Spring  examples/SpringApplicationContext/src/com/hcl/applicationcontext/SpringDemo.java
@@ -0,0 +1,18 @@
+package com.hcl.applicationcontext;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class SpringDemo {
+
+	public static void main(String[] args) {
+		//BeanFactory beanFactory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context=new  ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle=(Triangle)context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
