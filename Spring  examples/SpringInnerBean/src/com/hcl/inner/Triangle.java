cdde0edc5874de6e5240a12c9b54cf583a28f9f1
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-21 20:40:47 +0530

    Add files via upload

---
+++ Spring  examples/SpringInnerBean/src/com/hcl/inner/Triangle.java
@@ -0,0 +1,53 @@
+package com.hcl.inner;
+
+public class Triangle {
+private Point pointA;
+private Point pointB;
+private Point pointC;
+
+
+	
+	public Point getPointA() {
+	return pointA;
+}
+
+
+
+public void setPointA(Point pointA) {
+	this.pointA = pointA;
+}
+
+
+
+public Point getPointB() {
+	return pointB;
+}
+
+
+
+public void setPointB(Point pointB) {
+	this.pointB = pointB;
+}
+
+
+
+public Point getPointC() {
+	return pointC;
+}
+
+
+
+public void setPointC(Point pointC) {
+	this.pointC = pointC;
+}
+
+
+
+	public void draw() {
+		System.out.println("Point A is::"+(getPointA().getX()+","+getPointA().getY()));
+		System.out.println("Point B is::"+(getPointB().getX()+","+getPointB().getY()));
+		System.out.println("Point C is::"+(getPointC().getX()+","+getPointC().getY()));
+
+		
+	}
+}
