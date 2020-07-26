e344458c1cfc13b20fcf8760c80532eee6dea8e0
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-26 22:46:19 +0530

    Add files via upload

---
+++ springmvc/SpringMVCGlobelException/WebContent/WEB-INF/NullPointerException.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title></title>
+</head>
+<body>
+<h1>Sathyabama University</h1>
+<h3>Null pointer Exception occured .please contact customercare</h3>
+</body>
+</html>
---
+++ springmvc/SpringMVCGlobelException/src/com/Controller/Student.java
@@ -0,0 +1,52 @@
+package com.Controller;
+
+
+import java.util.Date;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+
+@JsonPropertyOrder({"studentDOB","studentMobile","studentHobby","Student_Name"})
+public class Student {
+	@Pattern(regexp="[^0-9]*" )
+	@JsonProperty("Student_Name")
+	private String studentName;
+	@Size(min=2,max=10)
+	private String studentHobby;
+	@Max(999)
+	private Long studentMobile;
+	@Past
+	private Date studentDOB;
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentHobby() {
+		return studentHobby;
+	}
+	public void setStudentHobby(String studentHobby) {
+		this.studentHobby = studentHobby;
+	}
+	public Long getStudentMobile() {
+		return studentMobile;
+	}
+	public void setStudentMobile(Long studentMobile) {
+		this.studentMobile = studentMobile;
+	}
+	public Date getStudentDOB() {
+		return studentDOB;
+	}
+	public void setStudentDOB(Date studentDOB) {
+		this.studentDOB = studentDOB;
+	}
+	
+}
